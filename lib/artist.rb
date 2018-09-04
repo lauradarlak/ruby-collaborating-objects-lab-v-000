58458494dcfa97441bfa5b25804e23179144de03
Laura Darlak <laura@polyporedesign.com>
2018-09-04 01:51:18 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,18 @@
 class Artist
-attr_accessor :name, :songs
+  attr_accessor :name, :songs
 
-def initialize(name)
-  @name = name
-  @songs = []
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def add_song(song)
-  @songs << song
-end
+  def add_song(song)
+    @songs << song
+  end
 
-def songs
-  @songs
-end
+  def songs
+    @songs
+  end
 
 
 
