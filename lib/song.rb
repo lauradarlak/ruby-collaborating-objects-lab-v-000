9e345fc458f394c8350a707c9eb58c7419bf3c47
Laura Darlak <laura@polyporedesign.com>
2018-09-04 02:14:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,10 @@ def initialize(name)
   @name = name
 end
 
-def artist_name(artist_name)
-  artist_name = Song.new(filename.split(" - ")[1])
-  self.artist = Artist.new(artist_name) if self.artist != artist_name
-  self.artist
-end
+def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(self)
+  end
 
 def self.new_by_filename(filename)
   song = self.new(filename.split(" - ")[1])
