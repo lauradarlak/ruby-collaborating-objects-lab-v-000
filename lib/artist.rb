a5cc244f113a8b83095378cacffe1ef0f21634e5
Laura Darlak <laura@polyporedesign.com>
2018-09-04 01:37:10 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,16 @@
 class Artist
-attr_accessor :name, :songs
+attr_accessor :name
+attr_reader :songs
 
 def initialize(name)
   @name = name
   @songs = []
 end
 
+def songs=(song)
+  @songs << song
+end
+
+  
 
 end
