76f6ac9224f80fd6f3d0f3e9d470eadd09c215fc
Laura Darlak <laura@polyporedesign.com>
2018-09-04 00:59:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,10 @@ def initialize(name)
   @name = name
 end
 
-# def self.artist_name(filename)
-#   artist_name = Song.new(filename.split(" - ")[0])
-#   @artist = Artist.new(artist_name)
-# end
+def self.artist_name(name)
+  artist_name = Song.new(filename.split(" - ")[0])
+  self.artist = Artist.new(artist_name)
+end
 
 def self.new_by_filename(filename)
   song = self.new
